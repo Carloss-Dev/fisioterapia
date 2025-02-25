1d6085165164b0757634f8307d4f77883259c7e2
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-02-25 11:43:08 -0300

    feat: deixando navbar modular
    adicionando nova biblioteca para icones

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,16 +1,36 @@
+import { Icon } from "@iconify-icon/react";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { NavLink } from "react-router-dom";
 
-interface NavigationItem {
+interface INavigationItemContent {
+  label: string;
+  route?: string;
+}
+interface INavigationItem {
   id: number;
-  triggerLabel: string;
-  content: string[];
+  itemLabel?: string;
+  itemRoute?: string;
+  triggerLabel?: string;
+  content?: INavigationItemContent[];
 }
 
-const navigationItems: NavigationItem[] = [
+const navigationItems: INavigationItem[] = [
   {
     id: 1,
     triggerLabel: "Cadastros",
-    content: ["Tags", "Públicos Alvos"],
+    content: [
+      {
+        label: "Tags",
+      },
+      {
+        label: "Publicos Alvos",
+      },
+    ],
+  },
+  {
+    id: 2,
+    itemLabel: "Github",
+    itemRoute: "/",
   },
 ];
 
@@ -20,20 +40,28 @@ export const Navbar = () => {
       <NavigationMenu.List className="flex space-x-6">
         {navigationItems.map((item) => (
           <NavigationMenu.Item key={item.id} className="relative">
-            <NavigationMenu.Trigger className="cursor-pointer px-4 py-2 font-bold tracking-wide text-white hover:text-neutral-200">
-              {item.triggerLabel}
-            </NavigationMenu.Trigger>
-            <NavigationMenu.Content className="absolute top-7 left-0 mt-2 w-40 rounded-md bg-white shadow-md">
-              <ul className="p-2">
-                {item.content.map((label) => (
-                  <li key={label} className="cursor-pointer">
-                    <NavigationMenu.Link className="block rounded-md p-2 hover:bg-gray-200">
-                      {label}
-                    </NavigationMenu.Link>
-                  </li>
-                ))}
-              </ul>
-            </NavigationMenu.Content>
+            {item.itemLabel ? (
+              <NavLink to={item.itemRoute ?? ""}> {item.itemLabel}</NavLink>
+            ) : (
+              <>
+                <NavigationMenu.Trigger className="inline-flex cursor-pointer px-4 py-2 font-bold tracking-wide text-white hover:text-neutral-200">
+                  {item.triggerLabel}
+                  <Icon icon="ri:arrow-down-s-line" className="self-center" />
+                </NavigationMenu.Trigger>
+                <NavigationMenu.Content className="absolute top-7 left-0 mt-2 w-40 rounded-md bg-white shadow-md">
+                  <ul className="p-2">
+                    {item.content?.map((itemContent) => (
+                      <li key={itemContent.label} className="cursor-pointer">
+                        <NavigationMenu.Link className="block rounded-md p-2 hover:bg-gray-200">
+                          {itemContent.label}
+                        </NavigationMenu.Link>
+                      </li>
+                    ))}
+                  </ul>
+                </NavigationMenu.Content>
+                itemContent
+              </>
+            )}
           </NavigationMenu.Item>
         ))}
       </NavigationMenu.List>
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,15 @@
+import { GridArea } from "@components/Layout/GridArea";
 import { Home } from "@screens/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <GridArea>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </GridArea>
     </BrowserRouter>
   );
 };
