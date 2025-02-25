1926e28c04810a03562427754d87c29ca1b166ca
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-02-25 11:09:59 -0300

    feat: estilização inicial concluída

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,12 +1,41 @@
-import { NavigationMenu } from "radix-ui";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+
+interface NavigationItem {
+  id: number;
+  triggerLabel: string;
+  content: string[];
+}
+
+const navigationItems: NavigationItem[] = [
+  {
+    id: 1,
+    triggerLabel: "Cadastros",
+    content: ["Tags", "Públicos Alvos"],
+  },
+];
 
 export const Navbar = () => {
   return (
-    <NavigationMenu.Root className="bg-primary col-start-13 col-end-9 row-span-1 flex items-center justify-end rounded-tl-2xl rounded-bl-2xl shadow-lg">
-      <NavigationMenu.List className="flex justify-center border-1 border-red-600 bg-white p-1">
-        <NavigationMenu.Item className="border-1 border-amber-500">
-          aidhasid
-        </NavigationMenu.Item>
+    <NavigationMenu.Root className="bg-primary flex h-full w-full items-center justify-center shadow-lg">
+      <NavigationMenu.List className="flex space-x-6">
+        {navigationItems.map((item) => (
+          <NavigationMenu.Item key={item.id} className="relative">
+            <NavigationMenu.Trigger className="cursor-pointer px-4 py-2 font-bold tracking-wide text-white hover:text-neutral-200">
+              {item.triggerLabel}
+            </NavigationMenu.Trigger>
+            <NavigationMenu.Content className="absolute top-7 left-0 mt-2 w-40 rounded-md bg-white shadow-md">
+              <ul className="p-2">
+                {item.content.map((label) => (
+                  <li key={label} className="cursor-pointer">
+                    <NavigationMenu.Link className="block rounded-md p-2 hover:bg-gray-200">
+                      {label}
+                    </NavigationMenu.Link>
+                  </li>
+                ))}
+              </ul>
+            </NavigationMenu.Content>
+          </NavigationMenu.Item>
+        ))}
       </NavigationMenu.List>
     </NavigationMenu.Root>
   );
