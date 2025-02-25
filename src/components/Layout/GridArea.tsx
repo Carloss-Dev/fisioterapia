fc39069d36936b56f53d74202f98a68be4b8a55a
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-02-25 11:09:59 -0300

    feat: estilização inicial concluída

--- src/components/Layout/GridArea.tsx
+++ src/components/Layout/GridArea.tsx
@@ -7,9 +7,13 @@ interface IGridArea {
 
 export const GridArea = ({ children }: IGridArea) => {
   return (
-    <main className="grid h-screen w-screen grid-cols-12 grid-rows-[100px_minmax(0,_1fr)_100px] bg-white pt-10">
-      <Navbar />
-      <section className="row-start-2 row-end-3">{children}</section>
+    <main className="grid h-screen w-screen grid-cols-12 grid-rows-[50px_1fr_100px] bg-white">
+      <header className="col-span-12 row-span-1">
+        <Navbar />
+      </header>
+      <section className="col-span-12 row-start-2 row-end-3 p-4">
+        {children}
+      </section>
     </main>
   );
 };
