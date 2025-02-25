cbeeeb98167815925b47ce2c480718568a5e80f5
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-02-25 15:06:58 -0300

    iniciando rotas

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -21,16 +21,13 @@ const navigationItems: INavigationItem[] = [
     content: [
       {
         label: "Tags",
+        route: "/dados/tags",
       },
       {
         label: "Publicos Alvos",
+        route: "/dados/publico-alvo",
       },
     ],
-  },
-  {
-    id: 2,
-    itemLabel: "Github",
-    itemRoute: "/",
   },
 ];
 
@@ -39,27 +36,42 @@ export const Navbar = () => {
     <NavigationMenu.Root className="bg-primary flex h-full w-full items-center justify-center shadow-lg">
       <NavigationMenu.List className="flex space-x-6">
         {navigationItems.map((item) => (
-          <NavigationMenu.Item key={item.id} className="relative">
+          <NavigationMenu.Item key={item.id} className="relative m-0">
             {item.itemLabel ? (
-              <NavLink to={item.itemRoute ?? ""}> {item.itemLabel}</NavLink>
+              <NavLink
+                to={item.itemRoute ?? ""}
+                className="flex cursor-pointer items-center justify-center px-4 py-2 font-bold tracking-wide text-white hover:text-neutral-200"
+              >
+                {" "}
+                {item.itemLabel}
+              </NavLink>
             ) : (
               <>
                 <NavigationMenu.Trigger className="inline-flex cursor-pointer px-4 py-2 font-bold tracking-wide text-white hover:text-neutral-200">
                   {item.triggerLabel}
-                  <Icon icon="ri:arrow-down-s-line" className="self-center" />
+
+                  <Icon
+                    icon="ri:arrow-down-s-line"
+                    className="self-center"
+                    aria-hidden
+                  />
                 </NavigationMenu.Trigger>
-                <NavigationMenu.Content className="absolute top-7 left-0 mt-2 w-40 rounded-md bg-white shadow-md">
+                <NavigationMenu.Content className="absolute top-10 w-40 rounded-md bg-white shadow-md">
                   <ul className="p-2">
                     {item.content?.map((itemContent) => (
                       <li key={itemContent.label} className="cursor-pointer">
                         <NavigationMenu.Link className="block rounded-md p-2 hover:bg-gray-200">
-                          {itemContent.label}
+                          <NavLink
+                            to={itemContent.route ?? ""}
+                            className="block"
+                          >
+                            {itemContent.label}
+                          </NavLink>
                         </NavigationMenu.Link>
                       </li>
                     ))}
                   </ul>
                 </NavigationMenu.Content>
-                itemContent
               </>
             )}
           </NavigationMenu.Item>
