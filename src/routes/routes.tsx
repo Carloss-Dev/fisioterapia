9c2bb3e7ef2f4ed6fbe6354c38a95c04ccdfdcc5
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-02-26 18:22:37 -0300

    feat: adicionando zod e hook form no projeto

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,8 @@
 import { GridArea } from "@components/Layout/GridArea";
 import { Home } from "@screens/Home";
 import { Page404 } from "@screens/Page404";
-import { Tags } from "@screens/Registers/Tags";
-import { TargetPublic } from "@screens/Registers/TargetPublic,";
+import { TagsRegister } from "@screens/Registers/TagsRegister";
+import { TargetAudienceRegister } from "@screens/Registers/TargetAudienceRegister";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export const AppRoutes = () => {
@@ -11,8 +11,11 @@ export const AppRoutes = () => {
       <GridArea>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/cadastro/tags" element={<Tags />} />
-          <Route path="/cadastro/publico-alvo" element={<TargetPublic />} />
+          <Route path="/cadastro/tags" element={<TagsRegister />} />
+          <Route
+            path="/cadastro/publico-alvo"
+            element={<TargetAudienceRegister />}
+          />
           <Route path="/*" element={<Page404 />} />
         </Routes>
       </GridArea>
