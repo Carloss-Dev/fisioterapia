e0bcccf6bd2be283c7416a2d2ae7e7532e2430f6
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-02-26 20:44:55 -0300

    criando um modal para os formulários

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,25 @@
+import type {
+  FieldError,
+  FieldValues,
+  Path,
+  UseFormRegister,
+} from "react-hook-form";
+
 type InputType = "text" | "password" | "email";
 
-interface IPropsInput {
+interface IPropsInput<T extends FieldValues> {
   id?: string;
-  name?: string;
+  name: Path<T>;
   label: string;
   type: InputType;
   placeholder?: string;
   className?: string;
   required?: boolean;
+  register: UseFormRegister<T>;
+  errors?: FieldError;
 }
 
-export const Input = ({
+export const Input = <T extends FieldValues>({
   id,
   label,
   placeholder,
@@ -18,17 +27,25 @@ export const Input = ({
   type,
   className,
   required,
-}: IPropsInput) => {
+  register,
+  errors,
+}: IPropsInput<T>) => {
   return (
     <div className={`flex flex-col gap-1 ${className}`}>
-      <label htmlFor={id} className="text-md font-bold tracking-wider">
+      <label
+        htmlFor={id}
+        className="text-md inline-flex justify-between font-bold tracking-wider"
+      >
         {label} {required && "*"}
+        <span className="block self-end text-sm text-red-500">
+          {errors?.message}
+        </span>
       </label>
       <input
         type={type}
         id={id}
         placeholder={placeholder}
-        name={name}
+        {...register(name, { required })}
         className="focus:border-primary ease h-11 w-full rounded-sm border-1 border-neutral-500 p-3 text-base outline-0 transition-all duration-100 focus:shadow-md"
       />
     </div>
