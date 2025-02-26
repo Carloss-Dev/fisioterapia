9f8e79cca568818d2e05ab20cb9f502c2fb9e28b
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-02-26 17:23:26 -0300

    feat:estilo do formulário, criação dos components

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,29 @@
+import type React from "react";
+
+type ButtonType = "submit" | "button" | "reset";
+
+interface IPropsButton {
+  children: React.ReactNode;
+  type?: ButtonType;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+}
+
+export const Button = ({
+  children,
+  className,
+  type,
+  onClick,
+}: IPropsButton) => {
+  return (
+    <div className={` ${className}`}>
+      <button
+        onClick={onClick}
+        type={type ? type : "button"}
+        className="bg-primary h-full w-full cursor-pointer rounded-sm font-bold text-white duration-200 hover:scale-105 hover:shadow-lg"
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
