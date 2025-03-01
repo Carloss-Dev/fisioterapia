ac42412c1d60ad1f77d12afa8a420bbefd4ba44f
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-03-01 12:24:28 -0300

    fix: atualizando imagem da page 404

--- src/screens/Page404.tsx
+++ src/screens/Page404.tsx
@@ -1,10 +1,12 @@
+import marioCorrendo from "/assets/mario-correndo.gif";
+
 export const Page404 = () => {
   return (
     <section className="col-span-12 flex flex-col items-center justify-center">
       <h2 className="text-4xl font-bold tracking-tight">
         404 Página não encontrada...
       </h2>
-      <img src="/assets/mario-correndo.gif" alt="" />
+      <img src={marioCorrendo} alt="" />
     </section>
   );
 };
