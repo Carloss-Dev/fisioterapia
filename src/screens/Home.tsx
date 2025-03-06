b81ea471656bb28579f3b9444b8582c67e0d66c6
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-03-06 20:36:07 -0300

    feat: componente de multiSelect

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,75 @@
+import { Button } from "@components/Button/Button";
+import { Input } from "@components/Input/Input";
+import { MultiSelect } from "@components/MultiSelect/MultiSelect";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  name: z.string().min(2, "O nome deve ter pelo menos 2 caracteres"),
+  tags: z.array(z.string()).optional(),
+});
+
+interface IFormData extends z.infer<typeof formSchema> {}
+
 export const Home = () => {
-  return <section>Home</section>;
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<IFormData>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (data: IFormData) => {
+    console.log("Dados do formulário:", data);
+  };
+
+  const handleTagsChange = (selectedTags: string[]) => {
+    setValue("tags", selectedTags);
+  };
+
+  return (
+    <section className="col-span-12 flex flex-col items-center justify-center gap-4 p-6">
+      <h1 className="text-xl font-bold">Formulário de Teste</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex w-96 flex-col gap-4"
+      >
+        <Input
+          label="Nome"
+          type="text"
+          id="name"
+          register={register}
+          errors={errors.name}
+          placeholder="Digite seu nome"
+          className="w-full"
+          {...register("name")}
+        />
+
+        {/* Passando a função handleTagsChange como onChange */}
+        <MultiSelect
+          options={[
+            "teste1",
+            "teste2",
+            "aoba",
+            "adasd",
+            "asdasasdsad",
+            "adihasidhas",
+            "aosidhasuiod",
+            "aosidhauis",
+            "sdugfdsui",
+            "asjidhasjkhd",
+          ]}
+          placeholder="Selecione tags..."
+          onChange={handleTagsChange}
+        />
+
+        <Button type="submit" className="w-full">
+          Enviar
+        </Button>
+      </form>
+    </section>
+  );
 };
