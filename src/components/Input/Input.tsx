65d096997a8800731bd214d929f6653e16c3aadd
Carloss-Dev <carlos2019cavalcante@gmail.com>
2025-03-06 20:36:07 -0300

    feat: componente de multiSelect

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,4 @@
+import type React from "react";
 import type {
   FieldError,
   FieldValues,
@@ -9,14 +10,18 @@ type InputType = "text" | "password" | "email";
 
 interface IPropsInput<T extends FieldValues> {
   id?: string;
-  name: Path<T>;
-  label: string;
+  name?: Path<T> | string;
+  label?: string;
   type: InputType;
+  disabled?: boolean;
   placeholder?: string;
   className?: string;
   required?: boolean;
-  register: UseFormRegister<T>;
+  register?: UseFormRegister<T>;
   errors?: FieldError;
+  icon?: React.ReactNode;
+  value?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 export const Input = <T extends FieldValues>({
@@ -26,9 +31,13 @@ export const Input = <T extends FieldValues>({
   name,
   type,
   className,
+  disabled,
   required,
   register,
   errors,
+  icon,
+  value,
+  onChange,
 }: IPropsInput<T>) => {
   return (
     <div className={`flex flex-col gap-1 ${className}`}>
@@ -43,15 +52,25 @@ export const Input = <T extends FieldValues>({
           </span>
         )}
       </label>
-      <input
-        type={type}
-        id={id}
-        placeholder={placeholder}
-        {...register(name, { required })}
-        className={
-          "focus:border-primary ease h-11 w-full rounded-sm border-1 border-neutral-500 p-3 text-base outline-0 transition-all duration-100 focus:shadow-md"
-        }
-      />
+      <div className="relative h-11 w-full">
+        <input
+          type={type}
+          id={id}
+          placeholder={placeholder}
+          disabled={disabled}
+          value={value}
+          {...(register
+            ? register(name as Path<T>, { required })
+            : { name, onChange })}
+          className={`focus:border-primary ease absolute h-full w-full rounded-sm border-1 border-neutral-500 p-3 text-base outline-0 transition-all duration-100 focus:shadow-md ${icon && "pr-12"} `}
+        />
+
+        {icon && (
+          <div className="absolute top-1/2 right-1 h-8 w-8 -translate-y-1/2">
+            {icon}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/components/MultiSelect/MultiSelect.tsx
@@ -0,0 +1,183 @@
+import { Input } from "@components/Input/Input";
+import { Icon } from "@iconify-icon/react";
+import { Popover, Separator, Tooltip } from "radix-ui";
+import React from "react";
+
+interface IMultiSelectProps {
+  options: string[];
+  placeholder?: string;
+  onChange?: (selected: string[]) => void;
+}
+
+export const MultiSelect = ({
+  options,
+  placeholder = "Selecione...",
+  onChange,
+}: IMultiSelectProps) => {
+  const [selected, setSelected] = React.useState<string[]>([]);
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  const [query, setQuery] = React.useState<string>("");
+  const maxVisibleTags = 3;
+
+  const filteredOptions = options.filter((option) =>
+    option.toLowerCase().includes(query.toLowerCase())
+  );
+
+  function toggleSelection(option: string) {
+    const newSelection = selected.includes(option)
+      ? selected.filter((item) => item !== option)
+      : [...selected, option];
+
+    setSelected(newSelection);
+
+    // Integração com o hookForm
+    onChange?.(newSelection);
+  }
+
+  const visibleTags = selected.slice(0, maxVisibleTags);
+  const hiddenTagsCount = selected.length - visibleTags.length;
+
+  return (
+    <div className="h-11 w-96 cursor-pointer">
+      <Popover.Root open={isOpen} onOpenChange={setIsOpen}>
+        <Popover.Trigger className="data-[state=open]:border-primary flex h-full w-full cursor-pointer items-center justify-between rounded-md border border-neutral-500 p-2 text-sm shadow-sm transition-all duration-100">
+          <div className="flex flex-wrap gap-2">
+            {visibleTags.length > 0 ? (
+              visibleTags.map((tag) => (
+                <div
+                  key={tag}
+                  className="flex cursor-pointer items-center justify-center gap-2 rounded-md border border-gray-500 bg-gray-200 py-1 ps-3 pe-1 transition-all duration-200 hover:scale-105 hover:shadow-md"
+                >
+                  <span className="max-w-12 overflow-hidden text-sm overflow-ellipsis whitespace-nowrap">
+                    {tag}
+                  </span>
+                  <Icon
+                    onClick={(e: React.MouseEvent) => {
+                      e.stopPropagation();
+                      toggleSelection(tag);
+                    }}
+                    icon="material-symbols:close"
+                    className="rounded-full p-1 text-sm text-gray-600 hover:bg-gray-300"
+                  />
+                </div>
+              ))
+            ) : (
+              <span className="text-sm text-gray-500">{placeholder}</span>
+            )}
+            {hiddenTagsCount > 0 && (
+              <Tooltip.Provider delayDuration={200}>
+                <Tooltip.Root>
+                  <Tooltip.Trigger asChild>
+                    <span className="flex cursor-pointer items-center gap-1 rounded-md bg-gray-200 px-2 py-1 text-xs text-gray-600">
+                      +{hiddenTagsCount}
+                    </span>
+                  </Tooltip.Trigger>
+                  <Tooltip.Content
+                    side="top"
+                    align="center"
+                    className="flex w-60 max-w-60 flex-wrap gap-2 rounded-md bg-white p-2 text-sm text-black shadow-md"
+                  >
+                    {selected.slice(maxVisibleTags).map((tag) => (
+                      <div
+                        key={tag}
+                        className="flex cursor-pointer items-center justify-center gap-2 rounded-md border border-gray-500 bg-gray-100 py-1 ps-3 pe-1 transition-all duration-200 hover:scale-105 hover:shadow-md"
+                      >
+                        <span className="max-w-12 overflow-hidden text-sm overflow-ellipsis whitespace-nowrap">
+                          {tag}
+                        </span>
+                        <Icon
+                          onClick={(e: React.MouseEvent) => {
+                            e.stopPropagation();
+                            toggleSelection(tag);
+                          }}
+                          icon="material-symbols:close"
+                          className="rounded-full p-1 text-sm text-gray-600 hover:bg-gray-300"
+                        />
+                      </div>
+                    ))}
+                    <Tooltip.Arrow className="fill-white" />
+                  </Tooltip.Content>
+                </Tooltip.Root>
+              </Tooltip.Provider>
+            )}
+          </div>
+          <Icon
+            icon="ep:arrow-down-bold"
+            className="cursor-pointer text-lg text-gray-500"
+          />
+        </Popover.Trigger>
+
+        <Popover.Content
+          align="start"
+          side="bottom"
+          className="z-50 w-56 rounded-md border border-neutral-500 bg-white p-2 shadow-lg"
+        >
+          <Input
+            type="text"
+            value={query}
+            placeholder="Buscar..."
+            className="h-10 text-sm"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <ul className="mt-2 max-h-40 overflow-y-auto">
+            {filteredOptions.length > 0 ? (
+              filteredOptions.map((option) => (
+                <li
+                  key={option}
+                  className="hover:text-primary hover:bg-primary/20 flex cursor-pointer items-center gap-2 rounded-md p-2 text-sm"
+                  onClick={() => toggleSelection(option)}
+                  onKeyDown={(e: React.KeyboardEvent) =>
+                    e.key === "Enter" && toggleSelection(option)
+                  }
+                >
+                  <input
+                    type="checkbox"
+                    checked={selected.includes(option)}
+                    onChange={() => toggleSelection(option)}
+                    className="text-primary h-5 w-5 cursor-pointer rounded border border-gray-400 focus:ring-0"
+                  />
+                  {option}
+                </li>
+              ))
+            ) : (
+              <li className="p-2 text-sm text-gray-500">
+                Nenhuma opção encontrada
+              </li>
+            )}
+          </ul>
+          <Separator.Root
+            orientation="vertical"
+            decorative
+            className="my-1 h-px w-full bg-gray-400"
+          />
+
+          <div className="flex h-fit w-full gap-1">
+            <button
+              onClick={() => setIsOpen(false)}
+              className="w-full cursor-pointer rounded-md p-2 hover:bg-gray-300"
+            >
+              Fechar
+            </button>
+
+            {selected.length > 0 && (
+              <>
+                <Separator.Root
+                  orientation="vertical"
+                  decorative
+                  className="h-5 w-px self-center bg-gray-400"
+                />
+                <button
+                  onClick={() => setSelected([])}
+                  className="w-full cursor-pointer rounded-md p-2 hover:bg-gray-300"
+                >
+                  Limpar
+                </button>
+              </>
+            )}
+          </div>
+          <Popover.Arrow className="fill-white" />
+        </Popover.Content>
+      </Popover.Root>
+    </div>
+  );
+};
